451c2e007cff00979d6250ace424291d460a2c9e
Netza López <l.lopeznetza@gmail.com>
2020-03-13 12:23:02 -0600

    Nuevas funciones
    guardarRestante actualizarPregunta guardarPresupuesto

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,61 @@
 import React, {useState} from 'react';
+import Error from './Error';
+import shortid from 'shortid';
 
-const Formulario = ()=>{
+
+const Formulario = ({agregarNuevoGasto})=>{
+
+    //Inicializar state
+    const [nombre, guardarNombre] = useState('');
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+    
+
+    //cuando el usuario agrega un gasto
+    const agregarGasto = e =>{
+        e.preventDefault();
+
+        //validar
+        if(cantidad < 1 || isNaN(cantidad) ){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        //construir el gasto 
+        const gasto={
+            nombre,
+            cantidad,
+            //se instala shortid >>npm i shortid
+            id: shortid.generate()
+        }
+        
+        //pasar el gasto a componente principal app
+        agregarNuevoGasto(gasto);
+
+
+        //resetear form
+        guardarNombre('');
+        guardarCantidad(0);
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={agregarGasto}
+        >
             <h2>Agrega tus gastos aquí</h2>
 
+            {error? <Error mensaje="Los 2 campos son obligatorios o 
+            el presupuesto es incorrecto"/> : null}
+            
             <div className="campo">
                 <label>Nombre Gasto</label>
                 <input
                     type="text"
                     className="u-full-width"
                     placeholder="Ej.Transporte"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
                 />
             </div>
 
@@ -20,6 +65,8 @@ const Formulario = ()=>{
                     type="number"
                     className="u-full-width"
                     placeholder="Ej. 300"
+                    value={cantidad}
+                    onChange={e=>guardarCantidad(parseInt(e.target.value,10))}
                 />
             </div>
 
